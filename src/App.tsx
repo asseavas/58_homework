882bc43aa0855b641c9922eb3e79e1924de3f7cb
Klimova Alina <alinaklimova82@gmail.com>
2024-06-11 17:14:50 +0600

    Lvl 1 done. Installed Bootstrap. A Modal component with a new button has been added and demonstrated on the page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
 import './App.css';
+import Modal from './Components/Modal/Modal';
+import {useState} from 'react';
 
 const App = () => {
+  const [showModal, setShowModal] = useState(false);
 
   return (
     <>
-
+      <div className="container-fluid">
+        <div className="row mt-2 justify-content-evenly">
+          <div className="col-5">
+            <button className="w-100 btn btn-primary" onClick={() => setShowModal(true)}>Click</button>
+            <Modal show={showModal} title="Hello" onClose={() => setShowModal(false)}>
+              <div className="modal-body">
+                Content
+              </div>
+              <div className="modal-footer">
+                <button className="btn btn-danger" onClick={() => setShowModal(false)}>Cancel</button>
+              </div>
+            </Modal>
+          </div>
+          <div className="col-5 ">
+            ALert
+          </div>
+        </div>
+      </div>
     </>
   );
 };
